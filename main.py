dbae9711e9584bd2fcd8c0deb9831ef909a48d79
Makoorr <azizbenrejeb@gmail.com>
2023-04-28 19:13:50 +0100

    changed main

--- main.py
+++ main.py
@@ -8,25 +8,6 @@ import sys
 import os
 
 
-# from PySide2.QtUiTools import QUiLoader
-
-# if __name__ == '__main__':
-#     # Some code to obtain the form file name, ui_file_name
-#     app = QApplication(sys.argv)
-#     ui_file = QFile(ui_file_name)
-#     if not ui_file.open(QIODevice.ReadOnly):
-#         print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
-#         sys.exit(-1)
-#     loader = QUiLoader()
-#     widget = loader.load(ui_file, None)
-#     ui_file.close()
-#     if not widget:
-#         print(loader.errorString())
-#         sys.exit(-1)
-#     widget.show()
-#     sys.exit(app.exec_())
-
-
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Ui_MainWindow, self).__init__()
@@ -34,11 +15,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         ui_path = os.path.dirname(os.path.abspath(__file__))
         uic.loadUi(os.path.join(ui_path, "ro.ui"), self)
 
-
-
-    
-
-app= QApplication(sys.argv)  # create application
-main= Ui_MainWindow()  # create ui object
+app = QApplication(sys.argv)  # create application
+main = Ui_MainWindow()  # create ui object
 main.show()  # show ui object
 sys.exit(app.exec_())  # execute application
