f18fba4d7b58596c7ac80caba7f638941cd2a629
Makoorr <azizbenrejeb@gmail.com>
2023-04-30 22:15:56 +0100

    updated functioning PL3

--- PLs/PL3.py
+++ PLs/PL3.py
@@ -1,45 +1,47 @@
+import numpy as np
 import gurobipy as gp
 
-# Initialiser les paramètres du problème
-jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-minimum_requis = [17, 13, 15, 19, 14, 16, 11]
-nb_jours = len(jours)
-nb_employes = 10
-jours_semaine = range(nb_jours)
-employes = range(nb_employes)
+class PL3:
+    def __init__(self,x1,x2,x3,x4,x5,x6,x7):
+        self.jours = [x1,x2,x3,x4,x5,x6,x7]
+        self.jour = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']
 
-# Initialiser le modèle
-model = gp.Model("Planification")
+    def run(self):
+        jours_de_conges= np.ones((7,7),dtype=int)
+        for k in range(7):
+            jours_de_conges[k,k] = 0
+            jours_de_conges[k,(k+1)%7] = 0
+        print(jours_de_conges)
 
-# Ajouter les variables
-travail = model.addVars(employes, jours_semaine, vtype=gp.GRB.BINARY, name="travail")
+        model = gp.Model("PL3")
 
-# Définir la fonction objective
-model.setObjective(gp.quicksum(travail[i, j] for i in employes for j in jours_semaine), gp.GRB.MINIMIZE)
+        # les variables de décision représentant les employés prenant deux jours de congés à partir du jour i
+        x=[]
+        for i in range(7):
+            x.append(model.addVar(lb = 0 ,vtype = gp.GRB.INTEGER, name='x'+str(i)))
 
-# Ajouter les contraintes
-# Chaque employé doit travailler pendant cinq jours consécutifs avant de prendre deux jours de congé
-for i in employes:
-    for j in range(nb_jours - 5):
-        model.addConstr(gp.quicksum(travail[i, k] for k in range(j, j+5)) == 5)
-        model.addConstr(gp.quicksum(travail[i, k] for k in range(j+5, j+7)) == 0)
-    model.addConstr(gp.quicksum(travail[i, k] for k in range(nb_jours-5, nb_jours)) == 5)
+        # la fonction objectif
+        model.setObjective(gp.quicksum(x), gp.GRB.MINIMIZE)
 
-# Les besoins en personnel doivent être satisfaits
-for j in jours_semaine:
-    model.addConstr(gp.quicksum(travail[i, j] for i in employes) >= minimum_requis[j])
+        # Contraintes : le nombre d'employé doit être >= au minimum requis pour un tel jour
+        for j in range(7):
+            model.addConstr(gp.quicksum(jours_de_conges[:,j] * x) >= self.jours[j])
 
-# Résoudre le modèle
-model.optimize()
+        # Resolution du PL
+        model.optimize()
 
-# Afficher les résultats
-if model.status == gp.GRB.OPTIMAL:
-    print("Solution optimale trouvée")
-    for i in employes:
-        print("Employé %d : " % i, end="")
-        for j in jours_semaine:
-            if travail[i, j].x > 0:
-                print("%s " % jours[j], end="")
-        print("")
-else:
-    print("Pas de solution optimale trouvée")
+        # Affichage des résultats
+        nb = []
+        for i,v in enumerate(model.getVars()):
+            nb.append(int(v.x))
+        
+        result = ""
+        for i in range(7):
+            val = model.objVal - ( nb[i] + nb[i-1] )
+            result += "%s: %d employés.\n" % (self.jour[i],val)
+        return result
+
+
+if "__main__" == __name__:
+    pl3 = PL3(17,13,15,19,14,16,11)
+    print(pl3.run())
