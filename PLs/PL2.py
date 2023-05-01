9da39305b5eb20666f26f96ede2bf5eeb645d1ac
Makoorr <azizbenrejeb@gmail.com>
2023-05-01 01:26:29 +0100

    updated PL2 to class

--- PLs/PL2.py
+++ PLs/PL2.py
@@ -1,36 +1,42 @@
 import gurobipy as gp
 
-# Initialisation des paramètres du problème
-niveau_qualite = [10, 5]  # Niveau de qualité pour chaque type de pétrole brut
-nb_barils = [5000, 10000]  # Nombre de barils pour chaque type de pétrole brut
-prix_vente = [25, 20]  # Prix de vente pour chaque produit final (gazoline, pétrole de chauffage)
-frais_marketing = [0.2, 0.1]  # Frais de marketing pour chaque produit final
+class PL2:
+    def __init__(self,niveau_qualite=[10, 5],nb_barils=[5000, 10000],prix_vente=[25, 20],frais_marketing=[0.2, 0.1]):
+        self.niveau_qualite = niveau_qualite
+        self.nb_barils = nb_barils
+        self.prix_vente = prix_vente
+        self.frais_marketing = frais_marketing
 
-# Initialisation du modèle
-model = gp.Model("PL2")
-model.params.NonConvex = 2
+    def run(self):
+        # Initialisation du modèle
+        model = gp.Model("PL2")
+        model.params.NonConvex = 2
 
-# Ajout des variables de décision
-gazoline = model.addVar(lb=0, ub=1, name="gazoline")
-chauffage = model.addVar(lb=0, ub=1, name="chauffage")
+        # Ajout des variables de décision
+        gazoline = model.addVar(lb=0, ub=1, name="gazoline")
+        chauffage = model.addVar(lb=0, ub=1, name="chauffage")
 
-# Fonction objectif : maximiser le profit total
-model.setObjective((prix_vente[0] - frais_marketing[0]) * gazoline + (prix_vente[1] - frais_marketing[1]) * chauffage, gp.GRB.MAXIMIZE)
+        # Fonction objectif : maximiser le profit total
+        model.setObjective((self.prix_vente[0] - self.frais_marketing[0]) * gazoline + (self.prix_vente[1] - self.frais_marketing[1]) * chauffage, gp.GRB.MAXIMIZE)
 
-# Contraintes
-d1 = model.addVar(0, name="denominateur1")
-d2 = model.addVar(0, name="denominateur2")
-model.addConstr( d1 == 1 / nb_barils[0]*gazoline + nb_barils[1]*chauffage )
-model.addConstr( d2 == 1 / nb_barils[0]*(1-gazoline) + nb_barils[1]*(1-chauffage) )
+        # Contraintes
+        d1 = model.addVar(0, name="denominateur1")
+        d2 = model.addVar(0, name="denominateur2")
+        model.addConstr( d1 == 1 / self.nb_barils[0]*gazoline + self.nb_barils[1]*chauffage )
+        model.addConstr( d2 == 1 / self.nb_barils[0]*(1-gazoline) + self.nb_barils[1]*(1-chauffage) )
 
-model.addConstr( ( (niveau_qualite[0]*nb_barils[0]*gazoline + niveau_qualite[1]*nb_barils[1]*chauffage) * d1 ) >= 8, name="c1")
-model.addConstr( ( (niveau_qualite[0]*nb_barils[0]*(1-gazoline) + niveau_qualite[1]*nb_barils[1]*(1-chauffage)) * d2) >= 6, name="c2")
+        model.addConstr( ( (self.niveau_qualite[0]*self.nb_barils[0]*gazoline + self.niveau_qualite[1]*self.nb_barils[1]*chauffage) * d1 ) >= 8, name="c1")
+        model.addConstr( ( (self.niveau_qualite[0]*self.nb_barils[0]*(1-gazoline) + self.niveau_qualite[1]*self.nb_barils[1]*(1-chauffage)) * d2) >= 6, name="c2")
 
-# Resolution
-model.optimize()
+        # Resolution
+        model.optimize()
 
-# Affichage des résultats
-print("Mixage optimal :")
-print("Gazoline :", gazoline.x)
-print("Pétrole de chauffage :", chauffage.x)
-print("Profit total :", model.objVal, "DT")
+        # Affichage des résultats
+        print("Mixage optimal :")
+        print("Gazoline :", gazoline.x)
+        print("Pétrole de chauffage :", chauffage.x)
+        print("Profit total :", model.objVal, "DT")
+
+if "__main__" == __name__:
+    pl2 = PL2()
+    pl2.run()
